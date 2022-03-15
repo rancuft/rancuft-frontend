0201af5719dd1a9cf172734b45ec291ddddb53d8
joungeun <lje0818ovo@naver.com>
2022-03-15 20:22:07 +0900

    [INIT] 리액트 설치

---
+++ rancuft/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import RootRouter from './route/RootRouter';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RootRouter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
