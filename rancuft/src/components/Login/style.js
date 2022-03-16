a3437f47bcc9c18ee6f887b0707d7810f6c00f01
joungeun <lje0818ovo@naver.com>
2022-03-16 17:46:15 +0900

    [ADD] #1 :: 버튼 말풍선 🗨

--- rancuft/src/components/Login/style.js
+++ rancuft/src/components/Login/style.js
@@ -1,27 +1,68 @@
 import styled from "@emotion/styled";
 
 export const Box = styled.div`
-input{
-  width: 136px;
-  height: 25px;
-  font-size: 10px;
-  margin: 0 0 40px 0;
-  padding-left: 5px;
-  border: solid 1.5px;
-  box-shadow: 1px 2px 5px 1px gray;
-}`;
+  input {
+    width: 136px;
+    height: 25px;
+    font-size: 10px;
+    margin: 0 0 40px 0;
+    padding-left: 5px;
+    border: solid 1.5px;
+    box-shadow: 1px 2px 4px 0.2px gray;
+  }
+`;
 
-export const SpeechBubble =  styled.div`
-input{
 
-}`;
+export const SpeechBubble = styled.div`
+  button {
+    position: relative;
+    width: 122px;
+    height: 25px;
+    font-size: 10px;
+
+    background: #ffffff;
+
+    border-radius: 15px;
+
+    box-shadow: 1px 2px 2px 0.2px gray;
+  }
+  ,
+  button:after {
+    
+    left: 98.5%;
+    top: 23.8%;
+    border: solid;
+    content: "";
+
+    position: absolute;
+    border-color: rgba(255, 255, 255, 0);;
+    border-left-color: #fff;
+
+    border-width: 6px;
+  }
+  ,
+  button:before {
+    left: 99.3%;
+    top: 15%;
+    border: solid;
+    content: "";
+
+    position: absolute;
+    border-color: rgba(0, 0, 0, 0);;
+    border-left-color: #000;
+
+    border-width: 7.5px;
+  }
+`;
 
 export const Background = styled.div`
-position: absolute;
-top: 50%;
-left: 50%;
-transform: translateX(-50%);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%);
 `;
 
 export const LoginBox = styled.div`
+  input {
+  }
 `;
