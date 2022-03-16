5b69860baad809465cd5f4fbcae93af079cb2128
joungeun <lje0818ovo@naver.com>
2022-03-16 10:52:50 +0900

    [ADD] #1 :: Id, password box style

--- rancuft/src/components/Login/style.js
+++ rancuft/src/components/Login/style.js
@@ -5,6 +5,10 @@ input{
   width: 136px;
   height: 25px;
   font-size: 10px;
+  margin: 0 0 40px 0;
+  padding-left: 5px;
+  border: solid 1.5px;
+  box-shadow: 1px 2px 5px 1px gray;
 }`;
 
 export const SpeechBubble =  styled.div`
