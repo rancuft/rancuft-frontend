8f0d79c87c9e07331daa1d4cef001ce9febf2f47
joungeun <lje0818ovo@naver.com>
2022-03-17 15:43:15 +0900

    [ADD] #1 :: 미디어 쿼리 테스트

--- rancuft/src/components/Login/index.js
+++ rancuft/src/components/Login/index.js
@@ -1,47 +1,70 @@
 import { useState } from "react";
-import * as S from "./style.js"
+import { useMediaQuery } from "react-responsive";
+import * as S from "./style.js";
 
 function Login() {
-    // const [inputNickname, setInputNickname] = useState('')
-    // const [inputPassword, setInputPassword] = useState('')
+  // const [inputNickname, setInputNickname] = useState('')
+  // const [inputPassword, setInputPassword] = useState('')
 
-    // input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
-    // const handleInputId = (e) => {
-    //     setInputId(e.target.value)
-    // }
- 
-    // const handleInputPw = (e) => {
-    //     setInputPw(e.target.value)
-    // }
- 
+  // input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
+  // const handleInputId = (e) => {
+  //     setInputId(e.target.value)
+  // }
 
+  // const handleInputPw = (e) => {
+  //     setInputPw(e.target.value)
+  // }
 
-    // 버튼 클릭 이벤트
-    const onClickLogin = () => {
-        console.log('click login')
-    }
-    const onClickGfitGive = () => {
-        console.log('click login')
-    }
+  // 버튼 클릭 이벤트
+  const onClickLogin = () => {
+    console.log("click login");
+  };
+  const onClickGfitGive = () => {
+    console.log("click login");
+  };
 
-    return(
+  const isMobile = useMediaQuery({
+    query: "(max-width:767px)",
+  });
+  const isPc = useMediaQuery({
+    query: "(min-width:768px)",
+  });
+
+  return (
+    <S.Background>
+      {isMobile && (
         <S.Background>
           <S.LoginBox>
             <S.Box>
-                <input type="text" name="input_nickname" placeholder="닉네임을 입력해 줄래요?"></input>
+              <input
+                type="text"
+                name="input_nickname"
+                placeholder="닉네임을 입력해 줄래요?"
+              ></input>
             </S.Box>
             <S.Box>
-                <input type="password" name="input_password" placeholder="비밀번호를 입력해 줄래요?"></input>
+              <input
+                type="password"
+                name="input_password"
+                placeholder="비밀번호를 입력해 줄래요?"
+              ></input>
             </S.Box>
             <S.SpeechBubble>
-                <button type="button" onClick={onClickLogin}>로그인 할래요?</button>
+              <button type="button" onClick={onClickLogin}>
+                로그인 할래요?
+              </button>
             </S.SpeechBubble>
           </S.LoginBox>
           <S.GiftGetSpeechBubble>
-              <button type="button" onClick={onClickGfitGive}>선물 <br></br>받을래요?</button>
+            <button type="button" onClick={onClickGfitGive}>
+              선물 <br></br>받을래요?
+            </button>
           </S.GiftGetSpeechBubble>
         </S.Background>
-    )
+      )}
+      {isPc && <h2>TEST</h2>}
+    </S.Background>
+  );
 }
 
 export default Login;
