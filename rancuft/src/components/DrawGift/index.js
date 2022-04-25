97adf599a723a5491c1030c2e280bf377febfbf5
joungeun <lje0818ovo@naver.com>
2022-04-25 22:54:55 +0900

    [ADD] #2 :: Canvas mouse click 동작 시 log

--- rancuft/src/components/DrawGift/index.js
+++ rancuft/src/components/DrawGift/index.js
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import * as S from "./style.js";
 
+
 function DrowGift() {
+
+  const canvas = useRef(null);
+
+  const [ctx, setCts] = useState(undefined);
+
+  useEffect(()=>{
+    setCts(canvas.current.getContext('2d'));
+
+  }, []);
+
   const isMobile = useMediaQuery({
     query: "(max-width:767px)",
   });
@@ -10,12 +21,29 @@ function DrowGift() {
     query: "(min-width:768px)",
   });
 
-  
+  const handlerMouseMove = e => {
+    if(ctx) {
+      var rect = canvas.current.getBoundingClientRect();
+      ctx.fillStyle = 'red';
+      console.log('ctx true')
+    }
+  }
+
   return (
+    // https://stickode.tistory.com/240 내가 원하는것!
     <>
       {isMobile && (                
       <S.FullBackground>
         <S.Background>
+          <S.DrawBox>
+          <canvas 
+          ref={canvas} 
+          width={800} height={800}
+          onMouseMove={handlerMouseMove}>이 브라우저는 캔버스를 지원하지 않습니다 😅<br/> 선물 받아보기는 어떠신가요?</canvas>
+          </S.DrawBox>
+          <S.Point>
+            <input type="range" min="0.1" max="5.0" step="0.1"/>
+          </S.Point>
           HI
         </S.Background>
        </S.FullBackground>
@@ -23,6 +51,10 @@ function DrowGift() {
       {isPc && <h2>TEST</h2>}
     </>
   );
+  
+  
 }
 
+
 export default DrowGift;
+
--- rancuft/src/components/DrawGift/style.js
+++ rancuft/src/components/DrawGift/style.js
@@ -15,3 +15,18 @@ export const FullBackground = styled.div`
   padding-bottom:200px;
   transform: translateX(50%);
 `;
+
+
+export const DrawBox = styled.div`
+  canvas{
+    width: 400px;
+    height: 350px;
+    background-color: black;
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  }
+
+`
+
+export const Point = styled.div`
+
+`
