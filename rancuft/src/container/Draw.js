c36015ed38ae30b471a522f427daf74b83ea3887
joungeun <lje0818ovo@naver.com>
2022-06-11 13:21:40 +0900

    [ADD] :: /draw 접근 제한 컨테이너에서

--- rancuft/src/container/Draw.js
+++ rancuft/src/container/Draw.js
@@ -1,10 +1,21 @@
 import Draw from "../components/DrawGift/index"
+import Login from "../components/Login/index"
 
 const DrawContainer = () => {
-    return( <>
-    <Draw />
-    </>
-    )
+    if(localStorage.getItem('Authorization')) {
+        return( <>
+            <Draw />
+            </>
+            )
+    } else {
+        return (
+            <>
+            <Login/>
+            </>
+        )
+    }
+
+    
 }
 
 export default DrawContainer;
